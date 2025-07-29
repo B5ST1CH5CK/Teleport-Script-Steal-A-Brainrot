c83c61573f4431c9e8891d677dbc921fb14479b4
B5ST1CH5CK <153829120+B5ST1CH5CK@users.noreply.github.com>
2025-07-29 12:12:19 +0300

    Create Teleport-Script-Steal-A-Brainrot-Rus-FriskHornet27-V2.lua

---
+++ Teleport-Script-Steal-A-Brainrot-Rus-FriskHornet27-V2.lua
@@ -0,0 +1,131 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("Укради Брэйнрот | Телепорт от B5ST1CH5CK", "RJTheme3")
+
+local Tab = Window:NewTab("База №1")
+
+local Section = Tab:NewSection("База №1")
+
+Section:NewButton("Этаж №1", "", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-317.88031005859375, -5.037245750427246, 114.92891693115234)
+end)
+
+Section:NewButton("Этаж №2", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-317.88031005859375, -7.015732288360596, 114.92891693115234)
+end)
+
+Section:NewButton("Этаж №2", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-317.88031005859375, -9.015732288360596, 114.92891693115234)
+end)
+
+local Tab = Window:NewTab("База №2")
+
+local Section = Tab:NewSection("База №2")
+
+Section:NewButton("Этаж №1", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-318.5852966308594, -5.037245750427246, 4.513580322265625)
+end)
+
+Section:NewButton("Этаж №2", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-318.5852966308594, -7.015732288360596, 4.513580322265625)
+end)
+
+Section:NewButton("Этаж №3", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-318.5852966308594, -9.015732288360596, 4.513580322265625)
+end)
+
+local Tab = Window:NewTab("База №3")
+
+local Section = Tab:NewSection("База №3")
+
+Section:NewButton("Этаж №1", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-316.49725341796875, -5.037245750427246, -97.6552963256836)
+end)
+
+Section:NewButton("Этаж №2", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-316.49725341796875, -7.015732288360596, -97.6552963256836)
+end)
+
+Section:NewButton("Этаж №3", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-316.49725341796875, -9.015732288360596, -97.6552963256836)
+end)
+
+local Tab = Window:NewTab("База №4")
+
+local Section = Tab:NewSection("База №4")
+
+Section:NewButton("Этаж №1", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-503.68194580078125, -5.0372467041015625, -101.17162322998047)
+end)
+
+Section:NewButton("Этаж №2", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-503.68194580078125, -7.015732288360596, -101.17162322998047)
+end)
+
+Section:NewButton("Этаж №3", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-503.68194580078125, -9.015732288360596, -101.17162322998047)
+end)
+
+local Tab = Window:NewTab("База №5")
+
+local Section = Tab:NewSection("База №5")
+
+Section:NewButton("Этаж №1", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-502.85028076171875, -5.0372467041015625, 7.733306884765625)
+end)
+
+Section:NewButton("Этаж №2", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-502.85028076171875, -7.015732288360596, 7.733306884765625)
+end)
+
+Section:NewButton("Этаж №3", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-502.85028076171875, -9.015732288360596, 7.733306884765625)
+end)
+
+local Tab = Window:NewTab("База №6")
+
+local Section = Tab:NewSection("База №6")
+
+Section:NewButton("Этаж №1", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-496.5442810058594, -5.0372467041015625, 112.71314239501953)
+end)
+
+Section:NewButton("Этаж №2", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-496.5442810058594, -7.015732288360596, 112.71314239501953)
+end)
+
+Section:NewButton("Этаж №3", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-496.5442810058594, -9.015732288360596, 112.71314239501953)
+end)
+
+local Tab = Window:NewTab("База №7")
+
+local Section = Tab:NewSection("База №7")
+
+Section:NewButton("Этаж №1", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-498.4517517089844, -5.0372467041015625, 218.56600952148438)
+end)
+
+Section:NewButton("Этаж №2", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-498.4517517089844, -7.015732288360596, 218.56600952148438)
+end)
+
+Section:NewButton("Этаж №3", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-498.4517517089844, -9.015732288360596, 218.56600952148438)
+end)
+
+local Tab = Window:NewTab("База №8")
+
+local Section = Tab:NewSection("База №8")
+
+Section:NewButton("Этаж №1", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-319.589508, -5.03724575, 220.6480260
+end)
+
+Section:NewButton("Этаж №2", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-319.589508, -7.015732288360596, 220.648026)
+end)
+
+Section:NewButton("Этаж №3", "", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-319.589508, -9.015732288360596, 220.648026)
+end)
